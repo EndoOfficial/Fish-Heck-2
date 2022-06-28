cdf45f0b56a0845481392618167824d6b0a3fc1b
zachary-coxsae <1026662@student.sae.edu.au>
2022-06-28 16:37:05 +1000

    3.8.1

--- Assets/Scripts/Shark/CoinSpawner.cs
+++ Assets/Scripts/Shark/CoinSpawner.cs
@@ -2,29 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class CoinSpawner : MonoBehaviour
 {
+    public GameObject Player;                   // spawn at player position if shark-free space not found
+
     public GameObject Platform;
     public GameObject Iceberg;
-    public Coin Coin;
+    public Coin Coin;                           // prefab
     private Coin currentCoin;
-    private Vector3 playArea;
+
+    // raycasting
     private float coinRadius;
-    private float raycastSize = 1.5f;
-    public LayerMask hitLayerMask;
-    private int activeCoin;
-    public int MaxCoins = 5;
+    private float raycastSize = 1.5f;           // to allow space between
+    private float rayCastHeight = 20f;
+    public LayerMask hitLayerMask;              // for raycast hits of sharks (and player?)
 
     private float widthx = 0;
     private float widthy = 0;
     private float widthz = 0;
     
     private Vector3 RandomSpawnPosition => new Vector3(UnityEngine.Random.Range(Platform.transform.position.x - (widthx/2f),
-                                                                                Platform.transform.position.x + (widthx / 2f)),
-                                                                                 (Platform.transform.position.y + 1),
-                                                                            UnityEngine.Random.Range(Platform.transform.position.z - (widthz / 2f),
+                                                                                    Platform.transform.position.x + (widthx / 2f)),
+                                                            (Platform.transform.position.y + 1),
+                                                            UnityEngine.Random.Range(Platform.transform.position.z - (widthz / 2f),
                                                                                     Platform.transform.position.z + (widthz / 2f)));
 
     private void OnEnable()
@@ -37,37 +39,48 @@ public class CoinSpawner : MonoBehaviour
         GameEvents.CoinScore -= SpawnCoin;
         GameEvents.CoinEat -= SpawnCoin;
     }
+
     private void Start()
     {
         widthx = Platform.transform.localScale.x;
         widthy = Platform.transform.localScale.y;
         widthz = Platform.transform.localScale.z;
+
+        coinRadius = Coin.GetComponent<BoxCollider>().size.x;           // STEVE: assumes 'Coin' (sushi) has a BoxColllider!
+        //coinRadius = Coin.GetComponent<SphereCollider>().radius;       // STEVE: if 'Coin' (sushi) has a SphereColllider
     }
+
+    // find a position away from the sharks (and player)
     private Vector3 VacantRandomPosition()
     {
         RaycastHit hit;
         Vector3 randomPosition;
-        int counter = 100; //limit duplication
+        int counter = 1000; //limit duplication
 
         do
         {
             randomPosition = RandomSpawnPosition;
 
-            Vector3 rayCastOrigin = new Vector3(randomPosition.x, randomPosition.y + 100, randomPosition.z);
+            Vector3 rayCastOrigin = new Vector3(randomPosition.x, randomPosition.y + rayCastHeight, randomPosition.z);
 
-            Physics.SphereCast(rayCastOrigin, coinRadius * raycastSize, Vector3.down, out hit, 200f, hitLayerMask);
+            Physics.SphereCast(rayCastOrigin, coinRadius * raycastSize, Vector3.down, out hit, rayCastHeight * 2f, hitLayerMask);
             counter--;
         }
         while (counter > 0 && hit.transform != null);
+
+        if (counter <= 0)
+        {
+            Debug.Log($"VacantRandomPosition: counter expired!!");
+            return Player.transform.position;
+        }
+
         return randomPosition;
-        
     }
+
     private void SpawnCoin()
     {
-      
-            currentCoin = Instantiate(Coin, transform);
-            currentCoin.transform.position = VacantRandomPosition();
-
+        currentCoin = Instantiate(Coin, transform);
+        currentCoin.transform.position = VacantRandomPosition();
     }
    
     private void OnDrawGizmos()
@@ -75,5 +88,4 @@ public class CoinSpawner : MonoBehaviour
         Gizmos.color = Color.white;
         Gizmos.DrawWireCube(Platform.transform.position, new Vector3(widthx, widthy, widthz));
     }
-
 }
